cc6aa2d2ff5482e948021d345e6dfc8778ea56e8
brad.r.willis <brad.r.willis@gmail.com>
2014-11-09 18:00:50 -0800

    Mentoring
    We got the sprite and section toggling to work properly.

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,41 +1,8 @@
 function handleBackgroundToggling(){
-  $( ".transition1" ).click(function() {
-    $(this).removeClass("ant-background");
-    $(".transition2").addClass("black-spider-background");
-    $(".transition3").addClass("cockroach-background");
-    $(".transition4").addClass("tick-background");
-    $(".transition5").addClass("tick-background");
-  });
-
-  $( ".transition2" ).click(function() {
-    $(this).removeClass("black-spider-background");
-    $(".transition1").addClass("ant-background");
-    $(".transition3").addClass("cockroach-background");
-    $(".transition4").addClass("tick-background");
-    $(".transition5").addClass("tick-background");
-  });
-
-  $( ".transition3" ).click(function() {
-    $(this).removeClass("cockroach-background");
-    $(".transition1").addClass("ant-background");
-    $(".transition2").addClass("black-spider-background");
-    $(".transition4").addClass("tick-background");
-    $(".transition5").addClass("tick-background");
-  });
-
-  $( ".transition4" ).click(function() {
-    $(this).removeClass("tick-background");
-    $(".transition1").addClass("ant-background");
-    $(".transition2").addClass("black-spider-background");
-    $(".transition3").addClass("tick-background");
-    $(".transition5").addClass("tick-background");
-  });
-
-  $( ".transition5" ).click(function() {
-    $(this).removeClass("tick-background");
-    $(".transition1").addClass("ant-background");
-    $(".transition2").addClass("black-spider-background");
-    $(".transition3").addClass("cockroach-background");
-    $(".transition4").addClass("tick-background");
+  $( ".insect-trap" ).click(function() {
+    $(".sprite-hidden").removeClass("sprite-hidden");
+    $(this).addClass("sprite-hidden");
+    $(".insect-trap-description-visible").removeClass("insect-trap-description-visible");
+    $("section",this).addClass("insect-trap-description-visible");
   });
 }
